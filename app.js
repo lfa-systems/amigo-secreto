bb5de46dd2aaadd432abdc9e56bf30c1ca514f0f
Luciano Azevedo <lucianocomputador@gmail.com>
2025-01-20 20:05:01 -0300

    Primeiro commit.

---
+++ app.js
@@ -0,0 +1,17 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.querySelector('input').value;
+    if ( amigo == '' ) {
+        alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(amigo);
+    }
+
+}
+
+function limparCampo() {
+    amigo = document.querySelector('input');
+    amigo.value = '';
+}
