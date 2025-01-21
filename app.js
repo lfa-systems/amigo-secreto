b7aaa9da98e79e6bb250952a9932564dab078eb1
Luciano Azevedo <lucianocomputador@gmail.com>
2025-01-20 21:55:53 -0300

    Função para sortear amigo da lista.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 let listaAmigos = document.getElementById( 'listaAmigos' );
-
+let resultado = document.getElementById( 'resultado' );
 
 function adicionarAmigo() {
     let amigo = document.querySelector( 'input' ).value;
     if ( amigo == '' ) {
-        alert('Por favor, insira um nome.');
+        alert( 'Por favor, insira um nome.' );
     } else {
         amigos.push( amigo );
     }
@@ -19,12 +19,20 @@ function limparCampo() {
     amigo.value = '';
 }
 
-
 function mostrarListaDeAmiigos() {
     listaAmigos.innerHTML = '';
     for ( let indice in amigos ){
        listaAmigos.innerHTML += '<li id=' + indice + '>' + amigos[indice] + '</li>';
     } 
+}
+
+function sortearAmigo() {
+    if ( !amigos.length ){
+        alert('Lista de amigos está vazio.')
+    } else {
+        let sorteadoIndice = Math.floor(Math.random() * amigos.length );
+        resultado.innerHTML = amigos[ sorteadoIndice ];
+    }
+
     
-    console.log(listaAmigos);
 }
