e503dd8e92361f297e17269614b9b54b853f4d80
Luciano Azevedo <lucianocomputador@gmail.com>
2025-01-20 21:39:04 -0300

    Função para atualizar a lista de amigos.

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
+let listaAmigos = document.getElementById( 'listaAmigos' );
+
 
 function adicionarAmigo() {
-    let amigo = document.querySelector('input').value;
+    let amigo = document.querySelector( 'input' ).value;
     if ( amigo == '' ) {
         alert('Por favor, insira um nome.');
     } else {
-        amigos.push(amigo);
+        amigos.push( amigo );
     }
+    mostrarListaDeAmiigos();
     limparCampo();
 }
 
 function limparCampo() {
-    amigo = document.querySelector('input');
+    amigo = document.querySelector( 'input' );
     amigo.value = '';
+}
+
+
+function mostrarListaDeAmiigos() {
+    listaAmigos.innerHTML = '';
+    for ( let indice in amigos ){
+       listaAmigos.innerHTML += '<li id=' + indice + '>' + amigos[indice] + '</li>';
+    } 
+    
+    console.log(listaAmigos);
 }
